e1669aa1a5512f52ba47f1adf1b12618cc4c2101
A SZCZYGIELSKI <andrzejsz@gmail.com>
2014-02-19 22:37:05 +0000

    commenting in action

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first, printing the list of students
 puts( "The students of recent cohort at Makers Academy")
 puts ("---------------------")
 puts("Andrew Johnson")
@@ -6,7 +7,8 @@ puts("John Ford")
 puts("Sam Cox")
 puts("Mark Spencer")
 puts("Alfred Tarski")
-
+# finally, printing the total
 print "Overall, we have "
+# it's important that print() doesn't add line characters
 print 9
 puts " great students"
