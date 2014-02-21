b4a6eef05e336cf9e90cf3cebe554b7c3d6ab984
A SZCZYGIELSKI <andrzejsz@gmail.com>
2014-02-21 11:39:57 +0000

    variables in use

--- directory.rb
+++ directory.rb
@@ -1,14 +1,12 @@
+student_count = 9
 # first, printing the list of students
-puts( "The students of recent cohort at Makers Academy")
-puts ("---------------------")
-puts("Andrew Johnson")
-puts("Kevin Dorking")
-puts("John Ford")
-puts("Sam Cox")
-puts("Mark Spencer")
-puts("Alfred Tarski")
+puts "The students of recent cohort at Makers Academy"
+puts "---------------------"
+puts"Andrew Johnson"
+puts"Kevin Dorking"
+puts"John Ford"
+puts"Sam Cox"
+puts"Mark Spencer"
+puts"Alfred Tarski"
 # finally, printing the total
-print "Overall, we have "
-# it's important that print() doesn't add line characters
-print 9
-puts " great students"
+puts "Overall, we have #{student_count} great students" 
